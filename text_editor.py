8dd324dd0dfa543867291b4c499c7ec69a678c8a
francisojeah <ojeahfrancis@gmail.com>
2024-01-05 07:55:55 +0100

    setup frame

--- text_editor.py
+++ text_editor.py
@@ -3,8 +3,14 @@ import tkinter as tk
 def main():
     window = tk.Tk()
     window.title("Text Editor")
+    window.rowconfigure(0,minsize=400)
+    window.columnconfigure(1,minsize=400)
+
 
     text_edit = tk.Text(window, font="Helvetica 18")
+    text_edit.grid(row=0, column=1)
+
+    fram= tk.Frame(window, relief=tk.RAISED, bd=2)
 
     window.mainloop()
 
